7041c90fa19d873d7adccf6a5a04f5ca7c43139b
Erick Luiz Fonseca Lopes <902139@lojasrenner.com.br>
2019-12-13 01:21:12 -0300

    Fix music model

--- models/Music.js
+++ models/Music.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema
 let MusicSchema = new Schema({
     title:{type: String, require: true, max:100, unique: true}, 
     letra:{type: String, require: true}, 
-    cifra: {type: String, require: true}
+    cifra: {type: String, require: true},
+    singer: {type: String, require: false},
+    style: {type: String, require: false},
+    enabled: {type: Boolean, defaultPrevented: true},
+    tom: {type: String}
 })
 
 module.exports = mongoose.model('Music', MusicSchema)
